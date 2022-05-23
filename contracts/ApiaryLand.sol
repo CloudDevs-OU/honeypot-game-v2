1e9d29881e37f191056961fb627d0f79441f84b3
rmamedov <rmamedov.work@gmail.com>
2022-05-23 11:01:09 +0300

    Merge ApiaryLand and ApiaryAccounting

--- contracts/ApiaryLand.sol
+++ contracts/ApiaryLand.sol
@@ -9,6 +9,10 @@ contract ApiaryLand {
         uint slots;
         uint[7] bees;
         uint[7] items;
+        uint lastClaimTimestamp;
+        uint lastDeferredPayoutTimestamp;
+        uint deferredProfit;
+        uint totalClaimedProfit;
     }
 
     // Constants
@@ -19,6 +23,14 @@ contract ApiaryLand {
     // State
     address public admin;
     mapping(address => Apiary) apiary;
+
+    // Configurable by admin
+    uint public moodRecoveryTime;
+    uint[7] beeDailyProfits = [1.5 ether, 3.5 ether, 7.5 ether, 25 ether, 45 ether, 95 ether, 200 ether];
+    mapping(uint => uint) itemBonusPercents;
+    mapping(uint => uint) setBonusPercents;
+    mapping(uint => uint) itemSet;
+    mapping(uint => uint[7]) setItems;
 
     // Modifiers
     modifier hasApiary(address account) {
@@ -33,6 +45,7 @@ contract ApiaryLand {
 
     constructor() {
         admin = msg.sender;
+        moodRecoveryTime = 7 days;
     }
 
     /**
@@ -45,6 +58,8 @@ contract ApiaryLand {
         require(apiary[account].owner == address(0), "Apiary is already created");
         apiary[account].owner = account;
         apiary[account].slots = DEFAULT_SLOTS;
+        apiary[account].lastClaimTimestamp = block.timestamp;
+        apiary[account].lastDeferredPayoutTimestamp = block.timestamp;
     }
 
     /**
@@ -57,26 +72,12 @@ contract ApiaryLand {
      */
     function addBees(address owner, uint[] memory beeIds, uint[] memory amounts) public onlyAdmin hasApiary(owner) {
         require(beeIds.length == amounts.length, "'beeIds' length not equal to 'amounts' length");
+        beforeApiaryStateChanged(owner);
         for(uint i; i < beeIds.length; i++) {
             apiary[owner].bees[beeIds[i] - 1] += amounts[i];
         }
 
         require(apiary[owner].slots >= getUsedSlots(owner), "Not enough slots");
-    }
-
-    /**
-     * @dev Set items to owner's apiary
-     * @notice Can be accessed only by contract admin
-     *
-     * @param owner Apiary owner
-     * @param beeIds array of bee ids
-     * @param itemIds array of item ids
-     */
-    function setItems(address owner, uint[] memory beeIds, uint[] memory itemIds) public onlyAdmin hasApiary(owner) {
-        require(beeIds.length == itemIds.length, "'beeIds' length not equal to 'itemIds' length");
-        for(uint i; i < beeIds.length; i++) {
-            apiary[owner].items[beeIds[i] - 1] = itemIds[i];
-        }
     }
 
     /**
@@ -91,6 +92,129 @@ contract ApiaryLand {
     }
 
     /**
+     * @dev Set items to owner's apiary
+     * @notice Can be accessed only by contract admin
+     *
+     * @param owner Apiary owner
+     * @param beeIds array of bee ids
+     * @param itemIds array of item ids
+     * @return item ids that no more in use (corresponding to beeIds)
+     */
+    function setApiaryItems(address owner, uint[] memory beeIds, uint[] memory itemIds) public onlyAdmin hasApiary(owner) returns(uint[] memory) {
+        require(beeIds.length == itemIds.length, "'beeIds' length not equal to 'itemIds' length");
+        beforeApiaryStateChanged(owner);
+        uint[] memory unusedItemIds = new uint[](beeIds.length);
+        for(uint i; i < beeIds.length; i++) {
+            if(apiary[owner].items[beeIds[i] - 1] > 0 && apiary[owner].items[beeIds[i] - 1] != itemIds[i]) {
+                unusedItemIds[i] = apiary[owner].items[beeIds[i] - 1];
+            }
+            apiary[owner].items[beeIds[i] - 1] = itemIds[i];
+        }
+
+        return unusedItemIds;
+    }
+
+    /**
+     * @dev Set new mood recovery time
+     * @notice Can be accessed only by contract admin
+     *
+     * @param _moodRecoveryTime New mood recovery time
+     */
+    function setMoodRecoveryTime(uint _moodRecoveryTime) public onlyAdmin {
+        moodRecoveryTime = _moodRecoveryTime;
+    }
+
+    /**
+     * @dev Set bee daily profits
+     * @notice Can be accessed only by contract admin
+     *
+     * @param _beeDailyProfits New bee daily profits
+     */
+    function setBeeDailyProfits(uint[7] memory _beeDailyProfits) public onlyAdmin {
+        beeDailyProfits = _beeDailyProfits;
+    }
+
+    /**
+     * @dev Save set with bonus percents and set items with bonus percents.
+     * If set was already saved then it will be updated.
+     *
+     * @notice Can be accessed only by contract admin
+     *
+     * @param setId Set identifier
+     * @param setBonusPercentage Bonus reward for collection all items from set (10000 = 100%)
+     * @param itemIds Item identifiers (10000 = 100%)
+     * @param itemBonusPercentage Item bonus percents corresponding to itemIds (10000 = 100%)
+     */
+    function saveSet(uint setId, uint setBonusPercentage, uint[7] memory itemIds, uint[7] memory itemBonusPercentage) public onlyAdmin {
+        setItems[setId] = itemIds;
+        setBonusPercents[setId] = setBonusPercentage;
+        for(uint i; i < itemIds.length; i++) {
+            itemBonusPercents[itemIds[i]] = itemBonusPercentage[i];
+            itemSet[itemIds[i]] = setId;
+        }
+    }
+
+    /**
+     * @dev Get set info
+     *
+     * @param setId Set identifier
+     * @return (setId, setBonusPercents, setItemsIds, setItemBonusPercents);
+     */
+    function getSet(uint setId) public view returns(uint, uint, uint[7] memory, uint[7] memory) {
+        uint[7] memory setItemBonusPercents;
+        for(uint i; i < TOTAL_BEES; i++) {
+            setItemBonusPercents[i] = itemBonusPercents[setItems[setId][i]];
+        }
+        return (setId, setBonusPercents[setId], setItems[setId], setItemBonusPercents);
+    }
+
+    /**
+     * @dev Add profit to owner's totalClaimedProfit and reset deferred payouts
+     * @notice Can be accessed only by contract admin
+     *
+     * @param owner Apiary owner
+     * @return claimed profit
+     */
+    function claimProfit(address owner) public onlyAdmin hasApiary(owner) returns(uint) {
+        uint profit = calcAvailableProfitForClaiming(owner, apiary[owner].bees, apiary[owner].items);
+        apiary[owner].totalClaimedProfit += profit;
+        apiary[owner].lastClaimTimestamp = block.timestamp;
+        apiary[owner].lastDeferredPayoutTimestamp = block.timestamp;
+        apiary[owner].deferredProfit = 0;
+
+        return profit;
+    }
+
+    /**
+     * @dev Function should be triggered before owner's apiary state changed
+     * in order to save deferred profit. It needs for correct calculation of profit that available for claiming.
+     *
+     * @notice Can be accessed only by contract admin
+     *
+     * @param owner Apiary owner
+     */
+    function beforeApiaryStateChanged(address owner) private onlyAdmin hasApiary(owner) {
+        apiary[owner].deferredProfit += calcPureProfit(apiary[owner].bees, apiary[owner].items, block.timestamp - apiary[owner].lastDeferredPayoutTimestamp);
+        apiary[owner].lastDeferredPayoutTimestamp = block.timestamp;
+    }
+
+    /**
+     * @dev Get setId by itemIds
+     *
+     * @param itemIds array of item ids
+     * @return setId or 0 if items not fro the same set
+     */
+    function getSetId(uint[7] memory itemIds) public view returns(uint) {
+        for(uint i = 1; i < itemIds.length; i++) {
+            if (itemSet[itemIds[i - 1]] != itemSet[itemIds[i]]) {
+                return 0;
+            }
+        }
+
+        return itemSet[itemIds[0]];
+    }
+
+    /**
      * @dev Get apiary by owner
      *
      * @param owner Apiary owner
@@ -99,7 +223,6 @@ contract ApiaryLand {
     function getApiary(address owner) public view returns(Apiary memory) {
         return apiary[owner];
     }
-
 
     /**
      * @dev Get owner's apiary used slots
@@ -123,5 +246,103 @@ contract ApiaryLand {
      */
     function getBeeSlots() public view returns(uint[] memory) {
         return beeSlots;
+    }
+
+    /**
+     * @dev Get bee daily profits
+     * @return array of bee daily profits (index = beeId - 1)
+     */
+    function getBeeDailyProfits() public view returns(uint[7] memory) {
+        return beeDailyProfits;
+    }
+
+    /**
+     * @dev Get apiary mood based on last claim timestamp.
+     * Mood factor affects to available for claiming profit and indicates
+     * how much percentage owner can get from pure profit.
+     *
+     * @param owner Apiary owner
+     * @return owner's apiary mood from -10000 up to +10000
+     */
+    function getApiaryMood(address owner) public view returns(int) {
+        uint timeSpent = block.timestamp - apiary[owner].lastClaimTimestamp;
+        if (timeSpent >= moodRecoveryTime) {
+            return int(10000);
+        }
+
+        return int(20000000 * timeSpent / moodRecoveryTime / 1000) - int(10000);
+    }
+
+    /**
+     * @dev Get item bonus percents by item ids
+     *
+     * @param itemIds array of item ids
+     * @return array of item bonus percents corresponding to itemIds (10000 = 100%)
+     */
+    function getItemBonusPercents(uint[] memory itemIds) public view returns(uint[] memory) {
+        uint[] memory result = new uint[](itemIds.length);
+        for(uint i; i < itemIds.length; i++) {
+            result[i] = itemBonusPercents[itemIds[i]];
+        }
+
+        return result;
+    }
+
+    /**
+     * @dev Get set bonus percents by set ids
+     *
+     * @param setIds array of set ids
+     * @return array of item bonus percents corresponding to setIds (10000 = 100%)
+     */
+    function getSetBonusPercents(uint[] memory setIds) public view returns(uint[] memory) {
+        uint[] memory result = new uint[](setIds.length);
+        for(uint i; i < setIds.length; i++) {
+            result[i] = setBonusPercents[setIds[i]];
+        }
+
+        return result;
+    }
+
+    /**
+     * @dev Calculate available profit for claiming (mood factor is applied)
+     *
+     * @param owner Apiary owner
+     * @param bees Owner bees
+     * @param items Owner items
+     * @return profit value
+     */
+    function calcAvailableProfitForClaiming(
+        address owner,
+        uint[7] memory bees,
+        uint[7] memory items
+    ) public view returns(uint) {
+        uint notDeferredProfit = calcPureProfit(bees, items, block.timestamp - apiary[owner].lastDeferredPayoutTimestamp);
+        // Apply mood factor
+        return (notDeferredProfit + apiary[owner].deferredProfit) * uint(int(10000) + getApiaryMood(owner)) / 10000;
+    }
+
+    /**
+     * @dev Calculate pure profit (mood factor is not applied)
+     *
+     * @param bees Owner bees
+     * @param items Owner items
+     * @param period period between start time and end time
+     * @return profit value
+     */
+    function calcPureProfit(
+        uint[7] memory bees,
+        uint[7] memory items,
+        uint period
+    ) public view returns(uint){
+        uint profit;
+
+        // Calc bees profit (+ items bonus)
+        for(uint i; i < bees.length; i++) {
+            // profit = dailyProfit * beesAmount * itemBonusPercent * daysSpent
+            profit += beeDailyProfits[i] * bees[i] * (10000 + itemBonusPercents[items[i]]) * period / 1 days / 10000;
+        }
+
+        // Apply set bonus
+        return profit * (10000 + setBonusPercents[getSetId(items)]) / 10000;
     }
 }
--- test/apiary-land-test.js
+++ test/apiary-land-test.js
@@ -102,12 +102,12 @@ describe("ApiaryLand", async function() {
 
     it("should fail to set items with message 'Only admin'", async function() {
         const [,owner] = await ethers.getSigners();
-        await expect(land.connect(owner).setItems(owner.address, [1], [1])).revertedWith('Only admin');
+        await expect(land.connect(owner).setApiaryItems(owner.address, [1], [1])).revertedWith('Only admin');
     })
 
     it("should fail to set items with message 'Must be apiary owner'", async function() {
         const [,,nonRegisteredAccount] = await ethers.getSigners();
-        await expect(land.setItems(nonRegisteredAccount.address, [1], [1])).revertedWith('Must be apiary owner');
+        await expect(land.setApiaryItems(nonRegisteredAccount.address, [1], [1])).revertedWith('Must be apiary owner');
     })
 
     it("should by default set item ids to 0", async function() {
@@ -122,9 +122,19 @@ describe("ApiaryLand", async function() {
         expect(apiary.items[6]).eq(0);
     })
 
+    it("should return array of zero values", async function() {
+        const [,owner] = await ethers.getSigners();
+        const unusedItems = await land.callStatic.setApiaryItems(owner.address, [1, 4, 7], [5, 9, 1]);
+        expect(unusedItems.length).eq(3);
+        expect(unusedItems[0]).eq(0);
+        expect(unusedItems[1]).eq(0);
+        expect(unusedItems[2]).eq(0);
+    })
+
     it("should successfully set item ids", async function() {
         const [,owner] = await ethers.getSigners();
-        await land.setItems(owner.address, [1, 4, 7], [5, 9, 1]);
+        await land.setApiaryItems(owner.address, [1, 4, 7], [5, 9, 1]);
+
         const apiary = await land.getApiary(owner.address);
         expect(apiary.items[0]).eq(5);
         expect(apiary.items[1]).eq(0);
@@ -135,9 +145,18 @@ describe("ApiaryLand", async function() {
         expect(apiary.items[6]).eq(1);
     })
 
+    it("should return array of none zero values", async function() {
+        const [,owner] = await ethers.getSigners();
+        const unusedItems = await land.callStatic.setApiaryItems(owner.address, [1, 4, 7], [3, 4, 1]);
+        expect(unusedItems.length).eq(3);
+        expect(unusedItems[0]).eq(5);
+        expect(unusedItems[1]).eq(9);
+        expect(unusedItems[2]).eq(0);
+    })
+
     it("should successfully update item ids", async function() {
         const [,owner] = await ethers.getSigners();
-        await land.setItems(owner.address, [1, 3, 7], [3, 4, 5]);
+        await land.setApiaryItems(owner.address, [1, 3, 7], [3, 4, 5]);
         const apiary = await land.getApiary(owner.address);
         expect(apiary.items[0]).eq(3);
         expect(apiary.items[1]).eq(0);
@@ -150,7 +169,7 @@ describe("ApiaryLand", async function() {
 
     it("should successfully reset item ids", async function() {
         const [,owner] = await ethers.getSigners();
-        await land.setItems(owner.address, [1, 3, 7], [0, 0, 0]);
+        await land.setApiaryItems(owner.address, [1, 3, 7], [0, 0, 0]);
         const apiary = await land.getApiary(owner.address);
         expect(apiary.items[0]).eq(0);
         expect(apiary.items[1]).eq(0);
@@ -159,5 +178,119 @@ describe("ApiaryLand", async function() {
         expect(apiary.items[4]).eq(0);
         expect(apiary.items[5]).eq(0);
         expect(apiary.items[6]).eq(0);
+    })
+
+    it("should set default moodRecoveryTime to 7 days", async function() {
+        const moodRecoveryTime = await land.moodRecoveryTime();
+        expect(moodRecoveryTime).eq(7*24*60*60);
+    })
+
+    it("should set moodRecoveryTime to 2 weeks", async function() {
+        const twoWeeks = 14*24*60*60;
+        await land.setMoodRecoveryTime(twoWeeks);
+        const moodRecoveryTime = await land.moodRecoveryTime();
+        expect(moodRecoveryTime).eq(twoWeeks);
+    })
+
+    it("should fail to update moodRecoveryTime with message 'Only admin'", async function() {
+        const [,, thirdPerson] = await ethers.getSigners();
+        await expect(land.connect(thirdPerson).setMoodRecoveryTime(0)).revertedWith("Only admin");
+    })
+
+    it("should set -10000 apiary mood for empty apiary", async function() {
+        const [, owner] = await ethers.getSigners();
+        const mood = await land.getApiaryMood(owner.address);
+        expect(mood).eq(-10000);
+    })
+
+    it("should fail to set new beeDailyProfits with message 'Only admin'", async function() {
+        const [,, thirdPerson] = await ethers.getSigners();
+        await expect(land.connect(thirdPerson).setBeeDailyProfits([0,0,0,0,0,0,0]))
+            .revertedWith("Only admin");
+    })
+
+    it("should set new beeDailyProfits", async function() {
+        const newBeeDailyProfits = [
+            ethers.utils.parseEther("1"),
+            ethers.utils.parseEther("2"),
+            ethers.utils.parseEther("3"),
+            ethers.utils.parseEther("4"),
+            ethers.utils.parseEther("5"),
+            ethers.utils.parseEther("6"),
+            ethers.utils.parseEther("7")
+        ];
+        await land.setBeeDailyProfits(newBeeDailyProfits);
+        const beeDailyProfits = await land.getBeeDailyProfits();
+
+        for (let i = 0; i < 7; i++) {
+            expect(beeDailyProfits[i]).eq(newBeeDailyProfits[i]);
+        }
+    })
+
+    it("should fail to save set with message 'Only admin'", async function() {
+        const [,, thirdPerson] = await ethers.getSigners();
+        await expect(land.connect(thirdPerson).saveSet(1, 100, [1,2,3,4,5,6,7], [1,2,3,4,5,6,7]))
+            .revertedWith("Only admin");
+    })
+
+    it("should successfully save set", async function() {
+        const [admin] = await ethers.getSigners();
+        await land.connect(admin).saveSet(1, 10000, [1,2,3,4,5,6,7], [1000,1000,1000,1000,1000,1000,1000]);
+        const [setId, setBonus, itemIds, itemBonuses] = await land.getSet(1);
+        expect(setId).eq(1);
+        expect(setBonus).eq(10000);
+        expect(itemIds[0]).eq(1);
+        expect(itemIds[1]).eq(2);
+        expect(itemIds[2]).eq(3);
+        expect(itemIds[3]).eq(4);
+        expect(itemIds[4]).eq(5);
+        expect(itemIds[5]).eq(6);
+        expect(itemIds[6]).eq(7);
+
+        itemBonuses.forEach(b => expect(b).eq(1000));
+    })
+
+    it("should successfully update set", async function() {
+        const [admin] = await ethers.getSigners();
+        await land.connect(admin).saveSet(1, 20000, [1,2,3,4,8,6,7], [700,700,700,700,700,700,700]);
+        const [setId, setBonus, itemIds, itemBonuses] = await land.getSet(1);
+        expect(setId).eq(1);
+        expect(setBonus).eq(20000);
+        expect(itemIds[0]).eq(1);
+        expect(itemIds[1]).eq(2);
+        expect(itemIds[2]).eq(3);
+        expect(itemIds[3]).eq(4);
+        expect(itemIds[4]).eq(8);
+        expect(itemIds[5]).eq(6);
+        expect(itemIds[6]).eq(7);
+
+        itemBonuses.forEach(b => expect(b).eq(700));
+    })
+
+
+    it("should correct calculate pure daily profit", async function() {
+        const profit = await land.calcPureProfit(
+            [1,0,0,0,0,0,0], // bees
+            [0,0,0,0,0,0,0], // items
+            24*60*60         // 1 day
+        )
+
+        const beeDailyProfits = await land.getBeeDailyProfits();
+        expect(profit).eq(beeDailyProfits[0]);
+    })
+
+    it("should correct calculate pure combined profit", async function() {
+        const profit = await land.calcPureProfit(
+            [1,1,3,4,0,0,0], // bees
+            [0,0,0,0,0,0,0], // items
+            12*24*60*60      // 12 days
+        )
+
+        const beeDailyProfits = await land.getBeeDailyProfits();
+        const firstBeeProfit = beeDailyProfits[0].mul(12)
+        const secondBeeProfit = beeDailyProfits[1].mul(12)
+        const thirdBeeProfit = beeDailyProfits[2].mul(12).mul(3)
+        const forthBeeProfit = beeDailyProfits[3].mul(12).mul(4)
+        expect(profit).eq(firstBeeProfit.add(secondBeeProfit).add(thirdBeeProfit).add(forthBeeProfit));
     })
 })
